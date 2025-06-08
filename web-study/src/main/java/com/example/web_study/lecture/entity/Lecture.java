f3cd3d4d8b2a12328082a15141d2c123638633e5
이건회 <rg970604@naver.com>
2025-06-09 00:11:57 +0900

    refactor: Lecture 테이블에 신청자 수와, 신청률을 포함하도록 반정규화

--- web-study/src/main/java/com/example/web_study/lecture/entity/Lecture.java
+++ web-study/src/main/java/com/example/web_study/lecture/entity/Lecture.java
@@ -1,6 +1,7 @@
 package com.example.web_study.lecture.entity;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -39,6 +40,12 @@ public class Lecture extends BaseEntity {
 	@Column(nullable = false, precision = 10, scale = 2)
 	private BigDecimal price;
 
+	@Column(nullable = false)
+	private int applicantCount;
+
+	@Column(nullable = false, precision = 5, scale = 2)
+	private BigDecimal applicationRate;
+
 	@Column(name = "users_id")
 	private Long userId;
 
@@ -49,8 +56,32 @@ public class Lecture extends BaseEntity {
 		this.title = title;
 		this.maxStudent = maxStudent;
 		this.price = price;
+		this.applicantCount = 0;
+		this.applicationRate = BigDecimal.ZERO;
 		this.userId = userId;
 	}
+
+	public void increaseApplicantCount() {
+		this.applicantCount++;
+		updateApplicationRate();
+	}
+
+	public void decreaseApplicantCount() {
+		if (this.applicantCount > 0) {
+			this.applicantCount--;
+			updateApplicationRate();
+		}
+	}
+
+	private void updateApplicationRate() {
+		if (this.maxStudent > 0) {
+			this.applicationRate = BigDecimal.valueOf(this.applicantCount * 100.0 / this.maxStudent)
+				.setScale(2, RoundingMode.HALF_UP);
+		} else {
+			this.applicationRate = BigDecimal.ZERO;
+		}
+	}
+
 
 	public void delete(){
 		this.isDeleted = true;
