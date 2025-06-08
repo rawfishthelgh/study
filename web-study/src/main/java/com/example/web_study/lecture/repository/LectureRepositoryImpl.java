a6c0174eb1f5fc033f5ccd11cf9370fa86ba5881
이건회 <rg970604@naver.com>
2025-06-09 00:28:38 +0900

    refactor: order by 조건을 최신순은 id, 신청자 순은 applicantCount, 신청률순은 applicationRate로 지정

--- web-study/src/main/java/com/example/web_study/lecture/repository/LectureRepositoryImpl.java
+++ web-study/src/main/java/com/example/web_study/lecture/repository/LectureRepositoryImpl.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.example.web_study.lecture.entity.QLecture;
-import com.example.web_study.lecture.entity.QLectureApplicantCount;
 import com.example.web_study.lecture.service.dto.LectureDto;
 import com.example.web_study.lecture.service.dto.LectureSortType;
 import com.example.web_study.user.entity.QUser;
@@ -26,7 +25,6 @@ public class LectureRepositoryImpl implements LectureRepositoryCustom {
 
 	QLecture lecture = QLecture.lecture;
 	QUser user = QUser.user;
-	QLectureApplicantCount applicantCount = QLectureApplicantCount.lectureApplicantCount;
 
 	@Override
 	public Page<LectureDto.Response> findAllBySort(LectureSortType sort, Pageable pageable) {
@@ -37,14 +35,12 @@ public class LectureRepositoryImpl implements LectureRepositoryCustom {
 				lecture.title,
 				lecture.price,
 				user.name.as("instructorName"),
-				applicantCount.applicantCount,
+				lecture.applicantCount,
 				lecture.maxStudent
 			))
 			.from(lecture)
 			.join(user)
 			.on(lecture.userId.eq(user.id))
-			.leftJoin(applicantCount)
-			.on(applicantCount.lecture.eq(lecture))
 			.orderBy(sortOrder(sort))
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize())
@@ -60,16 +56,13 @@ public class LectureRepositoryImpl implements LectureRepositoryCustom {
 
 	private OrderSpecifier<?> sortOrder(LectureSortType sort) {
 		if (sort == LectureSortType.POPULAR) {
-			return applicantCount.applicantCount.desc();
+			return lecture.applicantCount.desc();
 		}
 
 		if (sort == LectureSortType.RATE) {
-			return applicantCount.applicantCount
-				.doubleValue()
-				.divide(lecture.maxStudent.castToNum(Double.class))
-				.desc();
+			return lecture.applicationRate.desc();
 		}
 
-		return lecture.createdAt.desc();
+		return lecture.id.desc();
 	}
 }
