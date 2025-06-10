d13315fb19dff4030bd1844f1845149642c536f6
LEE_GEONHOE01 <lee_geonhoe01@eland.co.kr>
2025-06-10 17:08:04 +0900

    fix: price 0 허용

--- web-study/src/main/java/com/example/web_study/lecture/service/dto/LectureDto.java
+++ web-study/src/main/java/com/example/web_study/lecture/service/dto/LectureDto.java
@@ -1,18 +1,17 @@
 package com.example.web_study.lecture.service.dto;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
-
 import com.example.web_study.lecture.entity.Lecture;
 import com.example.web_study.user.entity.User;
-
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class LectureDto {
 
@@ -27,7 +26,7 @@ public class LectureDto {
         @Min(1)
         private int maxStudent;
 
-        @Positive
+        @PositiveOrZero
         private int price;
 
         public Lecture toEntity(User user) {
