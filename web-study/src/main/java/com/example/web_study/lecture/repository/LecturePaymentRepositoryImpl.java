74507a1a8b5cc76d86a15129952464d8ab1667d2
LEE_GEONHOE01 <lee_geonhoe01@eland.co.kr>
2025-06-10 15:31:13 +0900

    fix: 조인 테이블 결함 수정 및 where 조건 추가

--- web-study/src/main/java/com/example/web_study/lecture/repository/LecturePaymentRepositoryImpl.java
+++ web-study/src/main/java/com/example/web_study/lecture/repository/LecturePaymentRepositoryImpl.java
@@ -2,9 +2,11 @@ package com.example.web_study.lecture.repository;
 
 import java.util.List;
 
+import com.example.web_study.lecture.entity.QLecture;
 import com.example.web_study.lecture.entity.QLecturePayment;
 import com.example.web_study.lecture.service.dto.TopInstructorDto;
 import com.example.web_study.user.entity.QUser;
+import com.example.web_study.user.entity.UserType;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -13,26 +15,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LecturePaymentRepositoryImpl implements LecturePaymentRepositoryCustom {
 
-	private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory;
 
-	QLecturePayment lecturePayment = QLecturePayment.lecturePayment;
-	QUser user = QUser.user;
+    QLecture lecture = QLecture.lecture;
+    QLecturePayment lecturePayment = QLecturePayment.lecturePayment;
+    QUser user = QUser.user;
 
-	@Override
-	public List<TopInstructorDto> findTop10InstructorsByRevenue() {
-		return queryFactory
-			.select(Projections.constructor(
-				TopInstructorDto.class,
-				user.id,
-				user.name,
-				lecturePayment.amount.sum().as("totalRevenue")
-			))
-			.from(lecturePayment)
-			.join(user).on(user.id.eq(lecturePayment.lectureId))
-			.groupBy(user.id, user.name)
-			.orderBy(lecturePayment.amount.sum().desc())
-			.limit(10)
-			.fetch();
-	}
+    @Override
+    public List<TopInstructorDto> findTop10InstructorsByRevenue() {
+        return queryFactory
+                .select(Projections.constructor(
+                        TopInstructorDto.class,
+                        user.id,
+                        user.name,
+                        lecturePayment.amount.sum().as("totalRevenue")
+                ))
+                .from(lecturePayment)
+                .join(lecture).on(lecturePayment.lectureId.eq(lecture.id))
+                .join(user).on(user.id.eq(lecture.userId))
+                .where(user.userType.eq(UserType.INSTRUCTOR))
+                .groupBy(user.id, user.name)
+                .orderBy(lecturePayment.amount.sum().desc())
+                .limit(10)
+                .fetch();
+    }
 }
 
