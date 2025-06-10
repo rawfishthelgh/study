029ff58a717129197cfa4deff7bf18a417bd029b
LEE_GEONHOE01 <lee_geonhoe01@eland.co.kr>
2025-06-10 15:26:21 +0900

    chore: swagger 의존성 추가

---
+++ web-study/src/main/java/com/example/web_study/com/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.example.web_study.com.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "API 문서", version = "v1"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "Access Token만 입력하세요. 'Bearer'는 자동으로 추가됩니다."
+)
+public class OpenApiConfig {
+
+}
